3d26881c319aaad8a9cea9c7f35819295ab6df19
Nami Arjmandi <namiarjmandi@gmail.com>
2019-02-26 16:30:07 +0330

    Update bbb-du.sh

--- bbb-du.sh
+++ bbb-du.sh
@@ -2,13 +2,14 @@
 
 # Directories to show the sizes
 declare -a dirs=(
-        "/var/bigbluebutton"
-        "/var/bigbluebutton/published"
-        "/var/bigbluebutton/recording/raw"
-        "/var/bigbluebutton/recording/process"
-        "/var/freeswitch/meetings"
-        "/var/lib/red5/webapps/video/streams"
-        )
+	"/var/bigbluebutton/"
+	"/var/bigbluebutton/published/presentation/"
+	"/var/bigbluebutton/recording/raw/"
+	"/var/bigbluebutton/recording/process/"
+	"/var/freeswitch/meetings/"
+	"/var/lib/red5/webapps/video/streams/"
+	"/opt/freeswitch/log/"
+	)
 
 parameters="-s" # Default parameter for size
 
@@ -16,7 +17,7 @@ for par in "$@"
 do
         case "$par" in
                 -h|--human-readable) # Human readable dir size
-                        parameters="$parameters -h"
+                        parameters="$parameters -h" # du -h parameters for human readable output
                         ;;
                 *)
                         echo "Wrong parameter \"$par\""
